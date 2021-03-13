069dc64181dcc02804bde9826efebded19ae8b80
Püstə <puste.aghayeva@gmail.com>
2021-03-13 15:02:45 +0400

    firsthometask

---
+++ day1/index1.php
@@ -0,0 +1,39 @@
+<?php
+// Milad gecəsi pəncərənin qarşısında soldan sağa olmaqla üç gül: ətirşah, krokus və bənövşə düzülmüşdür. 
+// Maşa hər səhər pəncərəni silir və sağda dayanan güllə ortadakı gülün yerini dəyişdirirdi.
+// Tanya isə axşamlar gülləri sulayır və soldakı və ortadakı güllərin yerini dəyişdirirdi.
+// k-cı günün gecəsində güllərin düzülmə ardıcıllığını müəyyənləşdirmək tələb olunur.
+        // $x='G'; //ətirşah;
+        // $y='C'; //krokus;
+        // $z='V'; //bənövşə;
+        // for($n=1; $n<=7; $n++)
+        //     {
+        //         $a=$y; //=> C 
+        //         $y=$z; //=> V
+        //         $z=$a; //=> C
+        //         $b=$x; //=> G
+        //         $x=$y; //=> V
+        //         $y=$b; //=> G
+            
+        //     }
+        //     echo 'Güllərin düzülmə ardıcıllığı: '.$x.$y.$z;
+     
+            // $x=100;
+            // for($i=0; $i<=$x; $i++){
+            //   echo  $i= $i+1;
+            //   if($x%2 ==0){
+            //       echo 
+            //   }
+            
+              
+            // }
+            
+
+
+
+
+
+ ?>
+
+
+
